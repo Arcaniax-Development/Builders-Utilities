8025042614deebd6f0f7ae1968ad5359f81eefad
N0tMyFaultOG <mc.cache@web.de>
2020-09-08 20:25:16 +0200

    Apply license

--- src/main/java/net/arcaniax/buildersutilities/menus/SecretBlockMenuProvider.java
+++ src/main/java/net/arcaniax/buildersutilities/menus/SecretBlockMenuProvider.java
@@ -1,3 +1,27 @@
+/*
+ *      ____        _ _     _                      _    _ _   _ _ _ _   _
+ *     |  _ \      (_) |   | |                    | |  | | | (_) (_) | (_)
+ *     | |_) |_   _ _| | __| | ___ _ __ ___ ______| |  | | |_ _| |_| |_ _  ___  ___
+ *     |  _ <| | | | | |/ _` |/ _ \ '__/ __|______| |  | | __| | | | __| |/ _ \/ __|
+ *     | |_) | |_| | | | (_| |  __/ |  \__ \      | |__| | |_| | | | |_| |  __/\__ \
+ *     |____/ \__,_|_|_|\__,_|\___|_|  |___/       \____/ \__|_|_|_|\__|_|\___||___/
+ *
+ *    Builder's Utilities is a collection of a lot of tiny features that help with building.
+ *                          Copyright (C) 2020 Arcaniax
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.arcaniax.buildersutilities.menus;
 
 import net.arcaniax.buildersutilities.Main;
