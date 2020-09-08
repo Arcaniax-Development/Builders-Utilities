484a2f6fbaec5885770333325ec26dce8bd1f94f
N0tMyFaultOG <mc.cache@web.de>
2020-09-09 00:01:06 +0200

    Move from gradle to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.minecrell.gradle.licenser.LicenseExtension
+
+plugins {
+    id("java")
+    id("net.minecrell.licenser") version "0.4.1"
+    id("com.github.johnrengelman.shadow") version "6.0.0"
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        name = "Spigot"
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+    }
+    maven {
+        name = "bStats"
+        url = uri("https://repo.codemc.org/repository/maven-public")
+    }
+    maven {
+        name = "Mojang"
+        url = uri("https://libraries.minecraft.net/")
+    }
+}
+
+dependencies {
+    "compileOnly"("org.spigotmc:spigot-api:1.16.2-R0.1-SNAPSHOT")
+    "implementation"("com.mojang:authlib:1.5.25")
+    "implementation"("org.apache.logging.log4j:log4j-slf4j-impl:2.8.1")
+    "compile"("org.bstats:bstats-bukkit:1.7")
+}
+
+version = "2.0.0"
+
+tasks.named<ShadowJar>("shadowJar") {
+    dependencies {
+        include(dependency("org.apache.logging.log4j:log4j-slf4j-impl"))
+        include(dependency("org.slf4j:slf4j-api"))
+        include(dependency("org.bstats:bstats-bukkit:1.7"))
+        relocate("org.apache.logging.slf4j", "net.arcaniax.logging.apache")
+        relocate("org.slf4j", "net.arcaniax.logging.slf4j")
+        relocate("org.bstats", "net.arcaniax.metrics")
+    }
+}
+
+configure<LicenseExtension> {
+    header = rootProject.file("HEADER")
+    include("**/*.java")
+    exclude("**/XMaterial.java")
+
+}
+
+tasks.named<Copy>("processResources") {
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
--- src/main/java/net/arcaniax/buildersutilities/Main.java
+++ src/main/java/net/arcaniax/buildersutilities/Main.java
@@ -22,6 +22,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package net.arcaniax.buildersutilities;
 
 import net.arcaniax.buildersutilities.commands.system.CommandForwarder;
