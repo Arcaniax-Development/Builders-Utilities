bdf462582741f0488225cb9d6dff02c7da4053ea
NotMyFault <mc.cache@web.de>
2020-12-21 11:04:43 +0100

    bStats 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     "compileOnly"("org.spigotmc:spigot-api:1.16.4-R0.1-SNAPSHOT")
     "implementation"("com.mojang:authlib:1.5.25")
     "implementation"("org.apache.logging.log4j:log4j-slf4j-impl:2.8.1")
-    "compile"("org.bstats:bstats-bukkit:1.7")
+    "compile"("org.bstats:bstats-bukkit:1.8")
 }
 
 version = "2.0.0"
@@ -43,7 +43,7 @@ tasks.named<ShadowJar>("shadowJar") {
     dependencies {
         include(dependency("org.apache.logging.log4j:log4j-slf4j-impl"))
         include(dependency("org.slf4j:slf4j-api"))
-        include(dependency("org.bstats:bstats-bukkit:1.7"))
+        include(dependency("org.bstats:bstats-bukkit:1.8"))
         relocate("org.apache.logging.slf4j", "net.arcaniax.logging.apache")
         relocate("org.slf4j", "net.arcaniax.logging.slf4j")
         relocate("org.bstats", "net.arcaniax.metrics")
