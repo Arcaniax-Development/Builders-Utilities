d11b5ec9d211a0b635d18732e625245d03432ae5
NotMyFault <mc.cache@web.de>
2021-05-12 02:52:24 +0200

    Closes #35

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.ajoberstar.grgit.Grgit
 plugins {
     id("java")
     id("java-library")
-    id("org.cadixdev.licenser") version "0.5.1"
+    id("org.cadixdev.licenser") version "0.6.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("org.ajoberstar.grgit") version "4.1.0"
 }
@@ -95,8 +95,8 @@ tasks.named<ShadowJar>("shadowJar") {
 }
 
 configure<LicenseExtension> {
-    header = rootProject.file("HEADER")
-    newLine = false
+    header.set(resources.text.fromFile(file("HEADER.txt")))
+    newLine.set(false)
     include("**/*.java")
 }
 
